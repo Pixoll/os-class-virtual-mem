337f2ff92420560198d0f4866e6448323d2baf8f
Pixoll <pixoll.gamer@gmail.com>
2024-11-19 23:34:21 -0300

    Fix formatting

--- src/monitor_queue.hpp
+++ src/monitor_queue.hpp
@@ -1,21 +1,29 @@
 #pragma once
 
-#include <mutex>
 #include <condition_variable>
-#include <vector>
 #include <fstream>
-class MonitorQueue{
-private:
-    std::vector<int> queue;
-    int front,rear,items_stored,size,max_wait_time;
-    std::mutex mutex;
-    std::condition_variable producer_go;
-    std::condition_variable consumer_go;
-    std::fstream log_file;
-    void resize(int newsize);
+#include <mutex>
+#include <vector>
+
+class MonitorQueue {
+    std::vector<int> m_queue;
+    int m_front;
+    int m_rear;
+    int m_items_stored;
+    int m_size;
+    int m_max_wait_time;
+    std::mutex m_mutex;
+    std::condition_variable m_producer_go;
+    std::condition_variable m_consumer_go;
+    std::fstream m_log_file;
+
 public:
-    MonitorQueue(int initial_size);
+    explicit MonitorQueue(int initial_size);
+
     void set_log_file(std::fstream new_log_file);
     void push();
     int pop();
+
+private:
+    void resize(int new_size);
 };
