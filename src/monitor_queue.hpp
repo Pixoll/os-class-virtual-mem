b349dcaa783d8b83603eb448ff23df396f28b119
Pixoll <pixoll.gamer@gmail.com>
2024-11-19 23:58:12 -0300

    Initialize members

--- src/monitor_queue.hpp
+++ src/monitor_queue.hpp
@@ -6,23 +6,26 @@
 #include <vector>
 
 class MonitorQueue {
+    const int m_max_wait_time;
     std::vector<int> m_queue;
-    int m_front;
-    int m_rear;
-    int m_items_stored;
-    int m_size;
-    int m_max_wait_time;
     std::mutex m_mutex;
     std::condition_variable m_producer_go;
     std::condition_variable m_consumer_go;
     std::fstream m_log_file;
+    int m_head = 0;
+    int m_tail = 0;
+    int m_items_stored = 0;
+    int m_size = 0;
 
 public:
-    explicit MonitorQueue(int initial_size);
+    explicit MonitorQueue(const int initial_size, const int max_wait_time, const std::string &log_filename)
+        : m_max_wait_time(max_wait_time),
+          m_queue(initial_size),
+          m_log_file(log_filename) {}
 
-    void set_log_file(std::fstream new_log_file);
-    void push();
-    int pop();
+    // void push() {}
+    //
+    // int pop() {}
 
 private:
     void resize(int new_size);
